8a38dc52950f1979dc997f153866c9b3fcfbb6a2
tototete1 <45425004+tototete1@users.noreply.github.com>
2019-02-13 16:42:16 +0700

    Add files via upload

--- TAUBIEN.cpp
+++ TAUBIEN.cpp
@@ -6,7 +6,7 @@ using namespace std;
 long long a[10001],cx[10001];
 int main()
 {
-    long long n,m,i,s,j,day=0;
+    long long n,m,i,s,j,day1=0,day2=0;
     freopen("taubien.inp","r",stdin);
     ofstream fo ("taubien.out");
     scanf("%lld%lld",&n,&m);
@@ -30,10 +30,31 @@ int main()
                     }
                 }
             }
-            day++;
+            day1++;
         }
-
     }
-    fo <<day;
+    sort(a+1,a+1+n);
+    for (i=1;i<=n;i++)
+    {
+        s=a[i];
+        if (cx[i]==1)
+        {
+
+            cx[i]=0;
+            for (j=n;j>i;j--)
+            {
+                if (cx[j]==1)
+                {
+                    if (s+a[j]<=m)
+                    {
+                        s=s+a[j];
+                        cx[j]=0;
+                    }
+                }
+            }
+            day2++;
+        }
+    }
+    fo <<min(day1,day2);
     return 0;
 }
