b81ad3e52536d6d568f797175c1f103d5a042059
tototete1 <45425004+tototete1@users.noreply.github.com>
2019-02-18 14:39:02 +0700

    Update ROBOT.cpp

--- ROBOT.cpp
+++ ROBOT.cpp
@@ -3,19 +3,29 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-long long n,m,i,s1,s2,y,d,c,g,vt,s,x;
-long long r[1000001],v[1000001],L[1000001],R[1000001];
+long long i,s1,s2,y,d,c,g,vt,s,x;
+int m,n;
+long long L[1000001],R[1000001];
+int r[1000001],v[1000001];
 int main()
 {
     freopen("robot.inp","r",stdin);
-    scanf("%lld%lld",&n,&m);
-    for (i=1;i<=n;i++) scanf("%lld",&r[i]);
-    for (i=1;i<=m;i++) scanf("%lld",&v[i]);
+    scanf("%d%d",&n,&m);
+    for (i=1;i<=n;i++) scanf("%d",&r[i]);
+    for (i=1;i<=m;i++) scanf("%d",&v[i]);
     sort(v+1,v+1+m);
     L[1]=0;
-    for (i=2;i<=m;i++) L[i]=L[i-1]+(v[i]-v[i-1])*(i-1);
+    //for (i=2;i<=m;i++) L[i]=L[i-1]+(v[i]-v[i-1])*(i-1);
     R[m]=0;
-    for (i=m-1;i>0;i--) R[i]=R[i+1]+(v[i+1]-v[i])*(m-i);
+    //for (i=m-1;i>0;i--) R[i]=R[i+1]+(v[i+1]-v[i])*(m-i);
+    for (i=2;i<=m;i++)
+    {
+        L[i]=L[i-1]+(v[i]-v[i-1])*(i-1);
+        R[m-i+1]=R[m-i+2]+(v[m-i+2]-v[m-i+1])*(m-m+i-1);
+    }
+    /*for (i=1;i<=m;i++) cout <<L[i]<<" ";
+    cout <<endl;
+    for (i=m;i>0;i--) cout <<R[i]<<" ";*/
     for (i=1;i<=n;i++)
     {
         d=1,c=m,vt=0;
@@ -29,13 +39,13 @@ int main()
             }
             else c=g-1;
         }while (d<=c);
-        if (vt==m) y=0;
         s1=L[vt]+(r[i]-v[vt])*vt;
-        s2=R[vt+1]+(y)*(m-vt);
+        s2=R[vt+1]+(v[vt+1]-r[i])*(m-vt);
         s=s1+s2;
-        x=max(s,x);
+        if (s>x) x=s;
     }
-    freopen("robot.out","w",stdout);
-    printf("%lld",x);
+    ofstream fo ("robot.out");
+    //cout<<((double)(clock() - start)/CLOCKS_PER_SEC);
+    fo <<x;
     return 0;
 }
